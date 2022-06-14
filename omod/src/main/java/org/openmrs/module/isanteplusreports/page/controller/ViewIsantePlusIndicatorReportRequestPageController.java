78e9daeb9617acce3d6c77fcea398f4c70ae681f
HerbertYiga <herbertyiga@gmail.com>
2022-06-15 00:54:13 +0300

    fix

--- omod/src/main/java/org/openmrs/module/isanteplusreports/page/controller/ViewIsantePlusIndicatorReportRequestPageController.java
+++ omod/src/main/java/org/openmrs/module/isanteplusreports/page/controller/ViewIsantePlusIndicatorReportRequestPageController.java
@@ -85,7 +85,6 @@ public class ViewIsantePlusIndicatorReportRequestPageController {
 						model.addAttribute("request", requestUuid);
 						model.addAttribute("i", 0);
 						
-						
 					}
 				}
 			}
